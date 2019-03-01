54842f15f80528301f33e7b339a7ea2e6ce00774
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-01 00:45:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   #pigeon_list = {}
-  data.each_with_object({}) do |key, hash, result|
+  data.each_with_object({}) do |(key, hash), result|
 #binding.pry
 hash.each do |value, names|
   names.each do |name|
