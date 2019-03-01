757d7912afe4ce5834f760489ee4155f972f36b1
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-01 00:43:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,5 +6,10 @@ def nyc_pigeon_organizer(data)
 #binding.pry
 h.each do |value, names|
   names.each do |name|
+    result[name] ||= {}
+    result[name][key] ||= []
+    result[name][key] << value.to_s
+  end
+end
 end
 end
